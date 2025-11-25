ff1a1bdc421c18325262cd2eb63172d1ba7063b1
arunkumardigitaly <arunkumar.digitaly@gmail.com>
2025-11-25 16:28:40 +0530

    cors update

--- IMS1/settings.py
+++ IMS1/settings.py
@@ -83,6 +83,8 @@ def check_mysql_connection(request):
     
     
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -99,6 +101,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
+    'http://localhost:5173'
+    
     
 ]
 
